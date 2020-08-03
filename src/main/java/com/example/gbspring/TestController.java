b00d457e12fd33323031a1fec4e9d5edc13b23fd
Andrew <8wspace@gmail.com>
2020-08-03 14:10:54 +0300

    L1

---
+++ src/main/java/com/example/gbspring/TestController.java
@@ -0,0 +1,14 @@
+package com.example.gbspring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Test";
+    }
+
+}
